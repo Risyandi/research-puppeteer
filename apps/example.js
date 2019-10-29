54c93a58319879b549adf7b4a2fcbe983863be7c
risyandi <risyandi.94@gmail.com>
2019-10-29 14:52:59 +0700

    adding checking disconnected

--- apps/example.js
+++ apps/example.js
@@ -3,20 +3,27 @@
  * and save on the path previously have been set.
  */
 
+/** checking browser if disconnected 
+   if (browser.on('disconnected') != false) {
+       console.log("browser disconnected");
+   }
+*/
+
 const puppeteer = require('puppeteer');
 console.log(puppeteer, ":value of puppeteer");
 
 (async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
+    const version = await browser.version();
     console.log(browser, ":value of browser");
     console.log(page, ":value of page");
-    
+    console.log(version, ":value of version");
+
     await page.goto('https://opini.id');
     await page.screenshot({
         path: './screenshot/example-puppeteer-full-v2.png',
         fullPage: true
     });
-    
     await browser.close();
 })();
